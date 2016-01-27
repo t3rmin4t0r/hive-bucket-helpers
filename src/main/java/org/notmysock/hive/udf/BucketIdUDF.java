c8e2f8c898a34d4bb4bf8663434490ea6a18a87d
Gopal V <gopalv@apache.org>
2016-01-27 14:54:02 -0800

    simplest case

---
+++ src/main/java/org/notmysock/hive/udf/BucketIdUDF.java
@@ -0,0 +1,18 @@
+package org.notmysock.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.exec.Utilities;
+import org.apache.hadoop.hive.ql.udf.UDFType;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+@UDFType(deterministic = false)
+public class BucketIdUDF extends UDF {
+	transient IntWritable result = new IntWritable();
+	public final IntWritable evaluate(Text a) {
+		String filename = a.toString();
+		filename = filename.substring(filename.indexOf('/')+1);
+		result.set(Utilities.getBucketIdFromFile(filename));
+		return result;
+	}
+}
